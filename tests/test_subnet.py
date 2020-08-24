acc51d3bfd58efbc7deb1c00e61b59d9878c5ece
Rene Moser <mail@renemoser.net>
2020-08-24 09:36:16 +0200

    subnet: implement mutable

--- tests/test_subnet.py
+++ tests/test_subnet.py
@@ -13,6 +13,8 @@ SUBNET_RESP = {
         "uuid": "2db69ba3-1864-4608-853a-0771b6885a3a",
         "name": "my-network-name",
     },
+    "gateway_address": None,
+    "dns_servers": ["185.79.232.101", "185.79.232.102"],
     "tags": {}
 }
 
@@ -83,3 +85,147 @@ def test_subnet_missing_api_key():
         'list',
     ])
     assert result.exit_code == 1
+
+@responses.activate
+def test_subnets_delete():
+    uuid = "33333333-1864-4608-853a-0771b6885a3a"
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_ENDPOINT + '/subnets/' + uuid,
+        json=SUBNET_RESP,
+        status=200)
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_ENDPOINT + '/subnets/unknown',
+        json=SUBNET_RESP,
+        status=200)
+    responses.add(
+        responses.DELETE,
+        CLOUDSCALE_API_ENDPOINT + '/subnets/' + uuid,
+        status=204)
+    responses.add(
+        responses.DELETE,
+        CLOUDSCALE_API_ENDPOINT + '/subnets/unknown',
+        json={
+            "detail": "Not found."
+        },
+        status=404)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'delete',
+        uuid,
+    ])
+    assert result.exit_code == 1
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'delete',
+        '--force',
+        uuid,
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'delete',
+        '--force',
+        'unknown',
+    ])
+    assert result.exit_code > 0
+
+@responses.activate
+def test_subnets_create():
+    cidr = "192.0.2.123/24"
+    network_uuid = "2db69ba3-1864-4608-853a-0771b6885a3a"
+    dns_server1 = "185.79.232.101"
+    dns_server2 = "185.79.232.102"
+    responses.add(
+        responses.POST,
+        CLOUDSCALE_API_ENDPOINT + '/subnets',
+        json=SUBNET_RESP,
+        status=201)
+    responses.add(
+        responses.POST,
+        CLOUDSCALE_API_ENDPOINT + '/subnets',
+        json=SUBNET_RESP,
+        status=500)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'create',
+        '--cidr',
+        cidr,
+        '--network-uuid',
+        network_uuid,
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'create',
+        '--cidr',
+        cidr,
+        '--network-uuid',
+        network_uuid,
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+    ])
+    assert result.exit_code > 0
+
+@responses.activate
+def test_subnets_update():
+    uuid = "33333333-1864-4608-853a-0771b6885a3a"
+    dns_server1 = "185.79.232.101"
+    dns_server2 = "185.79.232.102"
+    responses.add(
+        responses.PATCH,
+        CLOUDSCALE_API_ENDPOINT + '/subnets/' + uuid,
+        json=SUBNET_RESP,
+        status=204)
+    responses.add(
+        responses.GET,
+        CLOUDSCALE_API_ENDPOINT + '/subnets/' + uuid,
+        json=SUBNET_RESP,
+        status=200)
+    responses.add(
+        responses.PATCH,
+        CLOUDSCALE_API_ENDPOINT + '/subnets/' + uuid,
+        json={},
+        status=500)
+
+    runner = CliRunner()
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'update',
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+        uuid,
+    ])
+    assert result.exit_code == 0
+    result = runner.invoke(cli, [
+        '-a', 'token',
+        'subnet',
+        'update',
+        '--dns-server',
+        dns_server1,
+        '--dns-server',
+        dns_server2,
+        '--clear-tag',
+        'foo',
+        uuid,
+    ])
+    assert result.exit_code > 0
