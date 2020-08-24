a01f33c16ce4731b258e12e7b350adbe40d3d860
René Moser <mail@renemoser.net>
2020-08-24 09:49:56 +0200

    subnet: implement mutable (#3)

--- cloudscale_cli/commands/subnet.py
+++ cloudscale_cli/commands/subnet.py
@@ -8,6 +8,8 @@ def subnet(ctx):
         'uuid',
         'cidr',
         'network',
+        'gateway_address',
+        'dns_servers',
         'tags',
     ]
 
@@ -27,4 +29,48 @@ def cmd_list(cloudscale, filter_tag, filter_json):
 def cmd_show(cloudscale, uuid):
     cloudscale.cmd_show(
         uuid=uuid,
+    )
+
+@click.option('--cidr', required=True)
+@click.option('--network-uuid', required=True)
+@click.option('--gateway-address')
+@click.option('--dns-server', 'dns_servers', multiple=True)
+@click.option('--tag', 'tags', multiple=True)
+@subnet.command("create")
+@click.pass_obj
+def cmd_create(cloudscale, cidr, network_uuid, gateway_address, dns_servers, tags):
+    cloudscale.cmd_create(
+        cidr=cidr,
+        network_uuid=network_uuid,
+        gateway_address=gateway_address,
+        dns_servers=dns_servers or None,
+        tags=tags,
+    )
+
+@click.argument('uuid', required=True)
+@click.option('--gateway-address')
+@click.option('--dns-server', 'dns_servers', multiple=True)
+@click.option('--tag', 'tags', multiple=True)
+@click.option('--clear-tag', 'clear_tags', multiple=True)
+@click.option('--clear-all-tags', is_flag=True)
+@subnet.command("update")
+@click.pass_obj
+def cmd_update(cloudscale, uuid, gateway_address, dns_servers, tags, clear_tags, clear_all_tags):
+    cloudscale.cmd_update(
+        uuid=uuid,
+        gateway_address=gateway_address,
+        dns_servers=dns_servers or None,
+        tags=tags,
+        clear_tags=clear_tags,
+        clear_all_tags=clear_all_tags,
+    )
+
+@click.argument('uuid', required=True)
+@click.option('--force', is_flag=True)
+@subnet.command("delete")
+@click.pass_obj
+def cmd_delete(cloudscale, uuid, force):
+    cloudscale.cmd_delete(
+        uuid=uuid,
+        force=force,
     )
